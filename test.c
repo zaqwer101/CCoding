61cf93e14c34bb9f038c71d39ca3b28c6a4b8e96
Your Name <you@example.com>
2016-09-13 21:27:20 +0800

    privet

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{	
+	struct Player { int X; int Y; int hp; int damage };
+	struct Player player;
+	system("clear");
+	
+	player.damage = 5;
+	player.hp = 20;
+	player.X = 10; 
+	player.Y = 10; 
+	
+	int directionX, directionY;
+	
+	changePosition(&player.X , &player.Y, -1, 1);
+	printf("%i, %i\n", player.X, player.Y);
+	
+	return 0;
+}
+
+void changePosition(int *playerX, int *playerY, int x, int y)
+{
+	*playerX += x;
+	*playerY += y;
+}

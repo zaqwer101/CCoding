0b87ab0efd5c5a841acefa86de5bf08877fb83b2
Your Name <you@example.com>
2016-09-14 19:09:32 +0800

    added smth

--- test.c
+++ test.c
@@ -2,27 +2,32 @@
 
 int main(int argc, char **argv)
 {	
-	struct Player { int X; int Y; int hp; int damage };
-	struct Player player;
 	system("clear");
+	struct Player { int hp; int damage; int lvl; int exp; int exp_to_lvl};
+	struct Player player, enemy; 
 	
-	player.damage = 5;
-	player.hp = 20;
-	player.X = 10; 
-	player.Y = 10; 
+	player.hp = 50; player.damage = 10; player.lvl = 1; player.exp = 0; player.exp_to_lvl = 100;
+	printf("Enemy hp = %i\nYour hp = %i", enemy.hp, player.hp);
 	
-	int directionX, directionY;
-	
-	changePosition(&player.X , &player.Y, -1, 1);
-	
-	printf("Current coodrds: [%i, %i]\n", player.X, player.Y);
+	while(true)
+	{
+		int enemy_level = 1;
+		system("clear");
+		enemy.lvl  = enemy_level; enemy.hp  = 10 * enemy.lvl; enemy.damage  =  5 * enemy.lvl;
+		printf("Enemy hp = %i\nYour hp = %i", enemy.hp, player.hp);
+		attack(&player.damage, &enemy.hp);
+		
+		if(enemy.hp <= 0) 
+		{
+			printf("Enemy is dead");
+		}
+
+	}
 	
 	return 0;
 }
 
-void changePosition(int *playerX, int *playerY, int x, int y)
+void attack(int *damage, int *enemyHp)
 {
-	*playerX += x;
-	*playerX += x;
-	*playerY += y;
+	*enemyHp -= *damage;
 }

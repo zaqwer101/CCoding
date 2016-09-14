981d415b4d99cf4003ba56d68a171fbfa68470fb
zaqwer <platonovza@ya.ru>
2016-09-14 22:32:24 +0800

    enemy defeated

--- test.c
+++ test.c
@@ -5,23 +5,23 @@ int main(int argc, char **argv)
 	system("clear");
 	struct Player { int hp; int damage; int lvl; int exp; int exp_to_lvl};
 	struct Player player, enemy; 
-	
+	int enemy_level = 1;
 	player.hp = 50; player.damage = 10; player.lvl = 1; player.exp = 0; player.exp_to_lvl = 100;
+	enemy.lvl  = enemy_level; enemy.hp  = 100 * enemy.lvl; enemy.damage  =  5 * enemy.lvl;
 	printf("Enemy hp = %i\nYour hp = %i", enemy.hp, player.hp);
 	
-	while(true)
+	while(1)
 	{
-		int enemy_level = 1;
-		system("clear");
-		enemy.lvl  = enemy_level; enemy.hp  = 10 * enemy.lvl; enemy.damage  =  5 * enemy.lvl;
-		printf("Enemy hp = %i\nYour hp = %i", enemy.hp, player.hp);
+		//system("clear");
+		printf("Enemy hp = %i\nYour hp = %i\n", enemy.hp, player.hp);
 		attack(&player.damage, &enemy.hp);
-		
+		printf("You attacked enemy\n");
 		if(enemy.hp <= 0) 
 		{
-			printf("Enemy is dead");
+			printf("Enemy is dead\n");
+			break;
 		}
-
+		//getchar();
 	}
 	
 	return 0;

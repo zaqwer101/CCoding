cf7f77c791fa8026d3d966c1e5c88ced38bd6de5
Your Name <you@example.com>
2016-09-16 15:23:25 +0800

    added ncurses

--- test.c
+++ test.c
@@ -1,33 +1,60 @@
 #include <stdio.h>
-
+#include <ncurses.h>
+struct Player { int hp; int damage; int lvl; int exp; int exp_to_lvl; char name[64]; };
+int current_line;
 int main(int argc, char **argv)
 {	
+	current_line = 2;
+	start_color();
+	init_pair(1, COLOR_BLUE, COLOR_WHITE);
 	system("clear");
-	struct Player { int hp; int damage; int lvl; int exp; int exp_to_lvl};
-	struct Player player, enemy; 
+	initscr();
+	int enemy_level = 1;
 	
-	player.hp = 50; player.damage = 10; player.lvl = 1; player.exp = 0; player.exp_to_lvl = 100;
-	printf("Enemy hp = %i\nYour hp = %i", enemy.hp, player.hp);
+	struct Player player = { 50, 10, 1, 0, 100, "Hero"};
+	struct Player enemy  = { 50, 10, 1, 0, 100, "Ogre"};
 	
-	while(true)
+	int turn = 1;
+	while(1)
 	{
-		int enemy_level = 1;
-		system("clear");
-		enemy.lvl  = enemy_level; enemy.hp  = 10 * enemy.lvl; enemy.damage  =  5 * enemy.lvl;
-		printf("Enemy hp = %i\nYour hp = %i", enemy.hp, player.hp);
-		attack(&player.damage, &enemy.hp);
-		
+		noecho();
+		int action;
+		draw_ui(&player.hp, &enemy.hp, turn);
 		if(enemy.hp <= 0) 
 		{
-			printf("Enemy is dead");
+			printw("%s is dead\n", enemy.name);
+			break;
 		}
-
+		refresh();
+		action = getch(); 
+		switch( action )
+		{
+			case 'a':
+				attack(&player, &enemy, turn);
+				turn++;
+				break;
+		}
 	}
-	
+	endwin();
+	printf("Game over in %i turns\n", turn);
 	return 0;
 }
 
-void attack(int *damage, int *enemyHp)
+void attack(struct Player *attacker, struct Player *defender, int turn)
 {
-	*enemyHp -= *damage;
+	defender->hp -= attacker->damage;
+	move(current_line, 5);
+	current_line++;
+	printw("%s attacked %s with %i damage", attacker->name, defender->name, attacker->damage);
+}
+
+void draw_ui(int *player_hp, int *enemy_hp, int turn_number)
+{
+	move(0,1);
+	printw("Your hp: %i", *player_hp)| COLOR_PAIR(1); 
+	move(0,30);
+	printw("Enemy hp: %i", *enemy_hp)| COLOR_PAIR(1);
+	move(0,60);
+	printw("Turn: %i", turn_number)| COLOR_PAIR(1);
+	refresh();
 }

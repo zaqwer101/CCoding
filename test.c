7b9b99ca87565c0f9130a9f2ff06970a707fbcc5
Your Name <you@example.com>
2016-09-24 19:43:18 +0800

    AI and healing added

--- test.c
+++ test.c
@@ -1,52 +1,71 @@
 #include <stdio.h>
 #include <ncurses.h>
-struct Player { int hp; int max_hp; int damage; int lvl; int exp; int exp_to_lvl; char name[64]; };
+struct Player { int hp; int max_hp; int damage; int lvl; int exp; int exp_to_lvl; char name[64]; int mana; int max_mana; int magic_power; };
 int current_line;
 int main(int argc, char **argv)
 {	
+	
 	WINDOW *log_win;
 	current_line = 2;
-	start_color();
-	init_pair(1, COLOR_BLUE, COLOR_WHITE);
 	system("clear");
 	initscr();
 	int enemy_level = 1;
 	
-	struct Player player = { 50, 50, 10, 1, 0, 100, "Hero"};
-	struct Player enemy  = { 5000, 5000, 10, 1, 0, 100, "Ogre"};
+	struct Player player = { 100,  100,   10,  1,  0,  100,  "Hero",  100,  100,  1};
+	struct Player enemy  = { 100, 100,    10,  1,  0,  100,  "Ogre",  100,  100,  1};
 	
 	int turn = 1;
 	while(1)
 	{
 		noecho();
-		int action;
-		draw_ui(&player.hp, &enemy.hp, turn);
-		if(enemy.hp <= 0) 
+		if(current_line >= 34)
 		{
-			//printw("%s is dead\n", enemy.name);
-			//break;
-		}
-		refresh();
-		action = getch(); 
-		switch( action )
-		{
-			case 'a':
-				if(enemy.hp > 0)
-				{
-					attack(&player, &enemy, turn);
-				}
-				turn++;
-				break;
-		}
-		
-		if(current_line == 34){
 			clear();
 			refresh();
 			current_line = 2;
 		}
+		int action;
+		draw_ui(&player.hp, &enemy.hp, turn);
+		if(enemy.hp <= 0) 
+		{
+			printw("%s is dead\n", enemy.name);
+			break;
+		}
+		refresh();
+		action = getch();
+		if(player.hp > 0)
+		{ 
+			switch( action )
+			{
+				case 'a':
+					if(enemy.hp > 0)
+					{
+						attack(&player, &enemy, turn);
+					}
+					turn++;
+					break;
+				case 'h':
+					heal(&player, turn);
+					turn++;
+					break;
+			}
+		}
+		else
+		{
+			break;
+		}
+		enemy_turn(&enemy, &player, turn);
+
 	}
 	endwin();
-	printf("Game over in %i turns\n", turn);
+	if(player.hp > 0)
+	{
+		printf("Game over in %i turns. You win\n", turn);
+	}
+	else
+	{
+		printf("Game over in %i turns. You dead\n", turn);
+	}
 	return 0;
 }
 
@@ -59,13 +78,27 @@ void attack(struct Player *attacker, struct Player *defender, int turn)
 	printw("%s attacked %s with %i damage", attacker->name, defender->name, attacker->damage);
 }
 
+void heal(struct Player *target, int turn)
+{
+	if(target->mana >= 25)
+	{
+		target->mana -= 25;
+		target->hp += 25 * target->magic_power;
+		move(current_line, 5);
+		current_line++;
+		printw("%s hp += %i", target->name, 25 * target->magic_power);
+	}
+}
+
 void draw_ui(struct Player *player, struct Player *enemy, int turn_number)
 {
 	move(0,1);
 	printw("HP: %i" , player->hp);
 	move(0,15);
-	printw("LVL: %i", player->lvl); 
+	printw("Mana: %i", player->mana);
 	move(0,30);
+	printw("LVL: %i", player->lvl); 
+	move(0,45);
 	printw("EXP: %i/%i", player->exp, player->exp_to_lvl);
 	move(0,60);
 	printw("Enemy HP: %i", enemy->hp);
@@ -82,6 +115,10 @@ void lvlup(struct Player *player)
 	player->max_hp += 50;
 	player->damage += 5;
 	player->hp = player->max_hp;
+	player->max_mana += 50;
+	player->mana = player->max_mana;
+	player->magic_power++;
+	
 	move(current_line, 5);
 	current_line++;
 	printw("%s lvlupped", player->name);
@@ -94,4 +131,17 @@ void add_exp(struct Player *player, int exp)
 	{
 		lvlup(player);
 	}
+}
+
+void enemy_turn(struct Player *enemy, struct Player *player, int turn)
+{
+	if(enemy->hp >= (enemy->hp / 2))
+	{
+		attack(enemy, player, turn);
+	}
+	else
+		if(enemy->mana >= 25)
+		{
+			heal(enemy,turn);
+		}
 }
